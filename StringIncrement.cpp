592b80926e3dbe8148286749f7a807eb2f5cb6c0
Maxim <87619450+OrlykM@users.noreply.github.com>
2022-01-09 23:49:59 +0200

    Create StringIncrement.cpp

---
+++ StringIncrement.cpp
@@ -0,0 +1,20 @@
+#include <vector>
+using namespace std;
+class Solution {
+public:
+    int finalValueAfterOperations(vector<string>& operations) {
+        int x = 0;
+        for (int i = 0; i < operations.size(); i++)
+        {
+            if (operations[i][1] == '+')
+            {
+                x++;
+            }
+            else if(operations[i][1] == '-')
+            {
+                x--;
+            }
+        }
+        return x;
+    }
+};
